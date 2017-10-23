218636a33278508c431d58868d03754b623e9aea
Pham Trong Tri <trongtri160592@yahoo.com>
2017-10-23 22:50:25 +0700

    Add background for footer

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,4 +12,4 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
-Rails.application.config.assets.precompile += %w( main.js main.css jquery.simplyscroll.css jquery.simplyscroll.js)
+Rails.application.config.assets.precompile += %w( main.js main.css.scss jquery.simplyscroll.css jquery.simplyscroll.js)
